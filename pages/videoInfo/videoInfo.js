05a881639ee5cc232126d2453e439db0b01ca1bc
nekosighed <lyl5913@163.com>
2019-09-12 16:55:07 +0800

    更新视频暂停切换

--- pages/videoInfo/videoInfo.js
+++ pages/videoInfo/videoInfo.js
@@ -8,7 +8,10 @@ Page({
 
   },
 
-  showSearch: function(){
+  // 存储 video 组件
+  theVideo: {},
+
+  showSearch: function() {
     wx.navigateTo({
       url: '../searchVideo/searchVideo',
     })
@@ -17,56 +20,60 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
+  onLoad: function(options) {
+    let that = this;
+    // 将 video 组件绑定到 theVideo 组件上
+    that.theVideo = wx.createVideoContext("myVideo", that);
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-
+  onShow: function() {
+    let that = this;
+    that.theVideo.play();
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
-
+  onHide: function() {
+    let that = this;
+    that.theVideo.pause();
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
